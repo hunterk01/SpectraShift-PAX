35a1493e323fd87086abb4bdd17c3624963cd0cf
hunterk01 <vudugrv@hotmail.com>
2016-07-13 19:25:11 -0700

    OctoBoss prototype model

---
+++ Assets/Scripts/AI/OctoBossGun.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OctoBossGun : LivingEntity
+{
+    public float health;
+
+    float hoverHeight;
+
+    PlayerController player;
+    GameObject playerTarget;
+    Rigidbody playerRB;
+    MovingEntity movingEntity;
+    WeaponSystems weaponSystems;
+
+
+    protected override void Start ()
+    {
+        base.Start();
+
+        player = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+        playerRB = player.GetComponent<Rigidbody>();
+        movingEntity = GetComponent<MovingEntity>();
+        playerTarget = GameObject.FindGameObjectWithTag("Player");
+        weaponSystems = GetComponent<WeaponSystems>();
+        hoverHeight = HeightManager.Instance.setHeight;
+
+        SetHeight();
+    }
+	
+	void Update ()
+    {
+        
+	}
+
+    void SetHeight()
+    {
+        transform.position = new Vector3(transform.position.x, hoverHeight, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/AI/OctoBossMain.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OctoBossMain : LivingEntity
+{
+    PlayerController player;
+    GameObject playerTarget;
+    Rigidbody playerRB;
+    MovingEntity movingEntity;
+    WeaponSystems weaponSystems;
+
+    protected override void Start ()
+    {
+        base.Start();
+
+        player = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+        playerRB = player.GetComponent<Rigidbody>();
+        movingEntity = GetComponent<MovingEntity>();
+        playerTarget = GameObject.FindGameObjectWithTag("Player");
+        weaponSystems = GetComponent<WeaponSystems>();
+    }
+	
+
+	void Update ()
+    {
+	
+	}
+}
